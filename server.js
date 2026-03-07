f2337317644471a306c330967f93f2b895d1a99d
Oscar Cerda <oskarcr.2019@gmail.com>
2026-03-07 12:11:44 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const port = process.env["SERVER_PORT"];
+const baseRoute = require("./baseRoute.js");
+const mongoose = require("mongoose");
+const url = process.env["DATABASE_URL"];
+
+/**
+ * Inicia el servidor!
+ */
+class Server {
+    static #started = false;
+
+    /**
+     * Intenta iniciar el servizor.
+     */
+    static async start() {
+        if(Server.#started) return;
+        Server.#started = true;
+        try {
+            await mongoose.connect(url);
+            console.log("Database connected sucessfully");
+        }
+        catch(err) {
+            console.log("Could not connect to the database");
+            console.error(err);
+            return;
+        }
+        Server.#configurate();
+        app.listen(port, () => {
+            console.log("App listeting on http://localhost:" + port);
+        });
+
+    }
+
+    /**
+     * Configura el servidor.
+     */
+    static #configurate() {
+        app.use((req, res, next) => {
+            res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+            res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+            res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+            next();
+        });
+        app.use("/static", express.static("public"));
+        app.use("/base", baseRoute);
+        app.get("/", (req, res) => res.send("Hello world!"));
+    }
+}
+
+Server.start();
