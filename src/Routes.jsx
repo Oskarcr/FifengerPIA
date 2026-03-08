378de18234e45d8f09e9c48a9ec249fdf4cb3e32
RILV <ricardo.lopezvrgs@uanl.edu.mx>
2026-03-08 15:56:34 -0600

    Sign and Log v2

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,13 @@
 import ChatList from "./pages/ChatList";
 import Login from "./pages/Login";
 import MenuList from "./pages/MenuList";
+import Signup from "./pages/Signup";
 
 const ROUTES_PAGES = {
     "chat_list": <ChatList/>,
     "menu_list": <MenuList/>,
-    "login": <Login/>
+    "login": <Login/>,
+    "signup": <Signup/>
 }
 
 /**
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+import getPage from "../Routes";
+
+
 export default function Login() {
+    const PAGE_NAME = "login"; 
+    const [pageName, setPage] = useState(PAGE_NAME);
+        if(pageName != PAGE_NAME) {
+            return getPage(pageName);
+        }
+
     return (
         <div id="container-wrapper">
             <div id="card-container">
                 <div id="card">
-                    <div id="login-title">Login in</div>
+                    <div id="login-title">Login In</div>
                     <input id="login-email" type="email" placeholder="Email"></input>
                     <input id="login-password" type="password" placeholder="Password"></input>
-                        <a id="login-account">Don't you have account?</a>
+                        <a id="login-account" onClick={() => setPage("signup")}>Don't you have account?</a>
                     <button id="login-button">Login</button>
                 </div>
             </div>
