8fbc57da32faf97816a0cb0773b85d0414e28970
RILV <ricardo.lopezvrgs@uanl.edu.mx>
2026-03-08 14:49:19 -0600

    Login and signup

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ChatList from './pages/ChatList.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChatList/>
+    <Login/>
   </StrictMode>,
 )
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,15 @@
+export default function Signup () {
+    return (
+        <div id="container-wrapper">
+            <div id="card-container">
+                <div id="card">
+                    <div id="signup-title">Signup</div>
+                    <input id="signup-username" type="text" placeholder="Username"></input>
+                    <input id="signup-email" type="email" placeholder="Email"></input>
+                    <input id="signup-password" type="password" placeholder="Password"></input>
+                    <button id="signup-button">Login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
