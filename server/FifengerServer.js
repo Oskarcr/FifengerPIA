5afa791ed7ba3ea468c7cdf6ffddf0a9abf0f94d
RILV <ricardo.lopezvrgs@uanl.edu.mx>
2026-05-23 20:25:30 -0600

    Token, cookies, middleware y cagada

--- server/FifengerServer.js
+++ server/FifengerServer.js
@@ -6,4 +6,5 @@ export { default as Attachments } from "./core/Attachments.js";
 export { default as ServerRoutes } from "./core/ServerRoutes.js";
 export { default as api } from "./core/api.js";
 export { default as app } from "./core/app.js";
+export { default as UserStatusEnum } from "./enums/enumerable.js"
 export { setEventsToSocket } from "./core/sockets.js";
